df0bdc280689326763080cc95a83c528dd8a3bdc
Oluwatobiloba <oluwateezzy03@gmail.com>
2023-08-08 13:53:40 +0100

    update

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 """duck-typed"""
-from typing import Any, Union, Sequence, Optional
+from typing import Any, Union, Sequence
 
 
 # The types of the elements of the input are not know
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """duck-typed"""
     if lst:
         return lst[0]
     else:
         return None
+    
