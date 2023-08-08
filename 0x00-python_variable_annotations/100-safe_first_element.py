c30eb0a2c76f122c430b8b750c9fbaf06b5bfc2d
Oluwatobiloba <oluwateezzy03@gmail.com>
2023-08-08 13:33:31 +0100

    update

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -4,7 +4,7 @@ from typing import Any, Union, Sequence, Optional
 
 
 # The types of the elements of the input are not know
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, Optional[None]]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     if lst:
         return lst[0]
     else:
