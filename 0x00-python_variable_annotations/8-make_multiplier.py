895a43c19b3722b05a3744332ad849422353b455
Oluwatobiloba <oluwateezzy03@gmail.com>
2023-08-08 13:49:09 +0100

    update

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -3,7 +3,8 @@
 from typing import Callable
 
 
-def make_multiplier(multiplier:float)->Callable[[float], float]:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """make multiplier"""
     def multiplier_function(value: float) -> float:
         return value * multiplier
     return multiplier_function
