0d5125d9ad36343d249d932779b48023b8c0fee1
Oluwatobiloba <oluwateezzy03@gmail.com>
2023-08-10 15:10:11 +0100

    update

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+""" wait random"""
 
 import asyncio
 from typing import List
--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -8,7 +8,7 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n:int, max_delay:int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     '''
         Test file for printing the correct output of the wait_n coroutine
     '''
