bc6e46f914c9ec34d3e3394ebc116fac3b68da32
Oluwatobiloba <oluwateezzy03@gmail.com>
2025-06-01 01:53:28 +0100

    Add unit test for public_repos method to validate correct repository retrieval

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -58,3 +58,32 @@ class TestGithubOrgClient(unittest.TestCase):
             client = GithubOrgClient("testorg")
             result = client._public_repos_url
             self.assertEqual(result, test_payload["repos_url"])
+    
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json: unittest.mock.Mock) -> None:
+        """Test that public_repos returns the correct list of repos."""
+
+        test_repos_payload = [
+            {"name": "repo1", "license": {"key": "mit"}},
+            {"name": "repo2", "license": {"key": "apache-2.0"}},
+            {"name": "repo3", "license": None},
+        ]
+        test_repos_url = "https://api.github.com/orgs/testorg/repos"
+
+        mock_get_json.return_value = test_repos_payload
+
+        with patch(
+            'client.GithubOrgClient._public_repos_url',
+                new_callable=PropertyMock
+            ) as mock_public_repos_url:
+            mock_public_repos_url.return_value = test_repos_url
+
+            client = GithubOrgClient("testorg")
+
+            repos = client.public_repos()
+
+            expected_repos = ["repo1", "repo2", "repo3"]
+            self.assertEqual(repos, expected_repos)
+
+            mock_public_repos_url.assert_called_once()
+            mock_get_json.assert_called_once_with(test_repos_url)
