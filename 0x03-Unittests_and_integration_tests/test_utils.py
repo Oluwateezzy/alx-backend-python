6fbedb4350f6de99d7e243caaea60c19a9f1ec81
Oluwatobiloba <oluwateezzy03@gmail.com>
2025-06-01 01:24:57 +0100

    Refactor test_memoize for improved readability by adjusting indentation and formatting of patch object

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -77,8 +77,11 @@ class TestMemoize(unittest.TestCase):
 
         test_instance = TestClass()
 
-        with patch.object(TestClass, 'a_method',
-                         return_value=42) as mock_method:
+        with patch.object(
+            TestClass,
+            'a_method',
+            return_value=42
+        ) as mock_method:
             result1 = test_instance.a_property
             result2 = test_instance.a_property
             self.assertEqual(result1, 42)
