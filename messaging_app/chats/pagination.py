55472518e70516feafff96b5c8c0be1b0e4498d8
Oluwatobiloba <oluwateezzy03@gmail.com>
2025-06-06 04:09:39 +0100

    Add total_count to paginated response in MessagePagination

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -8,6 +8,7 @@ class MessagePagination(PageNumberPagination):
     def get_paginated_response(self, data):
         response = super().get_paginated_response(data)
         response.data.update({
+            'total_count': self.page.paginator.count,
             'total_pages': self.page.paginator.num_pages,
             'current_page': self.page.number,
         })
