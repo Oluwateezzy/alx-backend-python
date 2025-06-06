5fbf6c30c20699f5a729558c9a5fc5fdf555bf5e
Oluwatobiloba <oluwateezzy03@gmail.com>
2025-06-06 01:58:11 +0100

    Add nested URL routing for conversations in chats app

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -4,8 +4,10 @@ from .views import ConversationViewSet, MessageViewSet
 
 router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
+conversations_router = routers.NestedDefaultRouter(router, r'conversations', lookup='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
 
 urlpatterns = [
+    path('', include(conversations_router.urls)),
     path('', include(router.urls)),
 ]
